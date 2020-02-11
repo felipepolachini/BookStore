4b914d6e8e25dc772dbcb6fae9a831946319257c
felipepolachini <fpiandoli@gmail.com>
2020-02-10 22:45:48 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,6 @@ require 'faker'
 require 'rspec'
 require 'httparty'
 require 'yaml'
-require 'report_builder'
 require_relative 'helper.rb'
 
 CONFIG = YAML.load_file("#{Dir.pwd}/config/config.yml")
