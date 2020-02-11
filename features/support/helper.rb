ff6d766fb8edd1dfbf1b5af4e8168ffa56eef3ef
felipepolachini <fpiandoli@gmail.com>
2020-02-10 22:46:06 -0300

    Update helper.rb

--- features/support/helper.rb
+++ features/support/helper.rb
@@ -4,8 +4,8 @@ module Helper
     
     path = "results/scrshoot/test_#{result}"
     screenshot = "#{path}/#{fileName}.png"
-#    page.save_screenshot(screenshot)
-#    embed(screenshot,'image/png','Click Here')
+    page.save_screenshot(screenshot)
+    embed(screenshot,'image/png','Click Here')
     
   end
   
